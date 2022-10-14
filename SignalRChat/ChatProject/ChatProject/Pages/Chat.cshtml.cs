0262eedad35b3be18ae3641537b9098fe97eb414
René Brøndum Christensen <rene723@gmail.com>
2022-10-14 13:41:24 +0200

    Splash og login sider done

---
+++ SignalRChat/ChatProject/ChatProject/Pages/Chat.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChatProject.Pages
+{
+    public class ChatModel : PageModel
+    {
+        private readonly ILogger<ChatModel> _logger;
+
+        public ChatModel(ILogger<ChatModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ SignalRChat/ChatProject/ChatProject/Pages/Login.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+
+namespace ChatProject.Pages.Shared
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        [BindProperty]
+        public string brugernavn { get; set; }
+        [BindProperty]
+        public string kodeord { get; set; }
+        public void OnPost()
+        {
+            // skriv kode der tester om det er en eksisterende bruger her.
+        }
+    }
+}
