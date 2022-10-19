a44231f7ee78b9b6ab22cb5702470f53d1f72929
René Brøndum Christensen <rene723@gmail.com>
2022-10-19 08:50:34 +0200

    Okay, so trying again to commit conMYsql class

---
+++ SignalRChat/ChatProject/ChatProject/Classerne/ConMysql.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+namespace ChatProject.Classes
+{
+    public class ConMysql
+    {
+        static readonly string cs = @"server=localhost;userid=dbuser;password=s$cret;database=testdb";
+        static readonly MySqlConnection Connect = new MySqlConnection(cs);
+
+        public void SendSqlQuery(string Message)
+        {
+            Connect.Open();
+            MySqlCommand Cmd = new MySqlCommand(Message, Connect);
+
+        }
+
+    }
+}
